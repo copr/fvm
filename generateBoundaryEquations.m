7a4316fd7f39c6ce97b55a446f8edeea0ceaab8e
Martin Kobersky <martinkobersky@gmail.com>
2015-03-05 18:04:50 +0100

    Presunute spocitani koeficientu v hranicnich rovnicich

--- generateBoundaryEquations.m
+++ generateBoundaryEquations.m
@@ -15,6 +15,12 @@ for j=1:nx % to by chtelo prepsat at je to bez foru a bez podminky
         De = allD(index, 1); Dw = allD(index, 2);
         Dn = allD(index, 3); Ds = allD(index, 4);
         
+        ae = max3(-Fe, De - Fe/2, 0); % podle knizky strana 124
+        aw = max3( Fw, Dw + Fw/2, 0); % kapitola 5.7.2
+        an = max3( Fs, Ds + Fs/2, 0); % je tohle hybridni diferencovani
+        as = max3(-Fn, Dn - Fn/2, 0); % chtel jsem to obecneji ale asi to nejde tak lehce
+        ap = ae + aw + an + as;
+        
         
         if (i == 1 || i == ny || j == 1 || j == nx) 
             index = (i-1)*nx + j; 
@@ -24,11 +30,6 @@ for j=1:nx % to by chtelo prepsat at je to bez foru a bez podminky
                     Mout(index, index) = 1;
                     vectorOut(index) = bounds.w;
                 else % jestli je na zapade neumann
-%                     ae = max3(-Fe, De - Fe/2, 0); % podle knizky strana 124
-%                     aw = max3( Fw, Dw + Fw/2, 0); % kapitola 5.7.2
-%                     an = max3( Fs, Ds + Fs/2, 0); % je tohle hybridni diferencovani
-%                     as = max3(-Fn, Dn - Fn/2, 0); % chtel jsem to obecneji ale asi to nejde tak lehce
-%                     ap = ae + aw + an + as;
 %                     
 %                     an = 0;
 %                     as = 0;
@@ -45,11 +46,6 @@ for j=1:nx % to by chtelo prepsat at je to bez foru a bez podminky
                     Mout(index, index) = 1;
                     vectorOut(index) = bounds.e;
                 else % jestli je na vychode neumann
-                    ae = max3(-Fe, De - Fe/2, 0); % podle knizky strana 124
-                    aw = max3( Fw, Dw + Fw/2, 0); % kapitola 5.7.2
-                    an = max3( Fs, Ds + Fs/2, 0); % je tohle hybridni diferencovani
-                    as = max3(-Fn, Dn - Fn/2, 0); % chtel jsem to obecneji ale asi to nejde tak lehce
-                    ap = ae + aw + an + as;
                     
                     an = 0;
                     as = 0;
@@ -66,11 +62,6 @@ for j=1:nx % to by chtelo prepsat at je to bez foru a bez podminky
                     Mout(index, index) = 1;
                     vectorOut(index) = bounds.s;
                 else % jestli je na jihode neumann
-                    ae = max3(-Fe, De - Fe/2, 0); % podle knizky strana 124
-                    aw = max3( Fw, Dw + Fw/2, 0); % kapitola 5.7.2
-                    an = max3( Fs, Ds + Fs/2, 0); % je tohle hybridni diferencovani
-                    as = max3(-Fn, Dn - Fn/2, 0); % chtel jsem to obecneji ale asi to nejde tak lehce
-                    ap = ae + aw + an + as;
                     
                     ae = 0;
                     as = 0;
@@ -87,11 +78,7 @@ for j=1:nx % to by chtelo prepsat at je to bez foru a bez podminky
                     Mout(index, index) = 1;
                     vectorOut(index) = bounds.n;
                 else % jestli je na severu neumann
-                    ae = max3(-Fe, De - Fe/2, 0); % podle knizky strana 124
-                    aw = max3( Fw, Dw + Fw/2, 0); % kapitola 5.7.2
-                    an = max3( Fs, Ds + Fs/2, 0); % je tohle hybridni diferencovani
-                    as = max3(-Fn, Dn - Fn/2, 0); % chtel jsem to obecneji ale asi to nejde tak lehce
-                    ap = ae + aw + an + as;
+
                     
                     an = 0;
                     ae = 0;
