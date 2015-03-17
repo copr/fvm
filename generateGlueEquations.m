6a7a44af1fd16f02cdad83e4920d0a2ae1b89874
Martin Kobersky <martinkobersky@gmail.com>
2015-03-18 00:37:37 +0100

    pridana moznost generovat slepovaci rovnice z obou stran

--- generateGlueEquations.m
+++ generateGlueEquations.m
@@ -1,7 +1,14 @@
-function [ M, vector ] = generateGlueEquations(M, vector, i, nx, ny )
+function [ M, vector ] = generateGlueEquations(M, vector, i, nx, ny, left )
 for j=1:nx
     index = (i-1)*nx + j;
-    line = assign(index, 1 , 0, 0, 1, 0, nx, ny);
+    if left
+        index2 = (i+1)*nx + j;
+    else
+        index2 = (i-3)*nx + j;
+    end
+    line = sparse(nx*ny, 1);
+    line(index) = 1;
+    line(index2) = -1;
     M(index, :) = line;
     vector(index) = 0;
 end
