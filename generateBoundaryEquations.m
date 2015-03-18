525de1f2cd771cf202ff38abfe4cd74bfa1da0f1
Martin Kobersky <martinkobersky@gmail.com>
2015-03-19 00:35:23 +0100

    zmenene maxi z max3 na normalni max funkci

--- generateBoundaryEquations.m
+++ generateBoundaryEquations.m
@@ -17,10 +17,10 @@ for j=1:nx
         De = allD(index, 1); Dw = allD(index, 2);
         Dn = allD(index, 3); Ds = allD(index, 4);
         
-        ae = max3(-Fe, De - Fe/2, 0); % podle knizky strana 124
-        aw = max3( Fw, Dw + Fw/2, 0); % kapitola 5.7.2
-        an = max3( Fs, Ds + Fs/2, 0); % je tohle hybridni diferencovani
-        as = max3(-Fn, Dn - Fn/2, 0); % chtel jsem to obecneji ale asi to nejde tak lehce
+        ae = max([-Fe, De - Fe/2, 0]); % podle knizky strana 124
+        aw = max([ Fw, Dw + Fw/2, 0]); % kapitola 5.7.2
+        an = max([ Fs, Ds + Fs/2, 0]); % je tohle hybridni diferencovani
+        as = max([-Fn, Dn - Fn/2, 0]); % chtel jsem to obecneji ale asi to nejde tak lehce
         ap = ae + aw + an + as;
         
 %         if (i == 1 && j == 1 || i == 1 && j == nx || ...
--- generateNonBoundaryEquations.m
+++ generateNonBoundaryEquations.m
@@ -17,10 +17,10 @@ for j=startJ:endJ
         De = allD(index, 1); Dw = allD(index, 2);
         Dn = allD(index, 3); Ds = allD(index, 4);
         
-        ae = max3(-Fe, De - Fe/2, 0); % podle knizky strana 124 
-        aw = max3( Fw, Dw + Fw/2, 0); % kapitola 5.7.2
-        an = max3( Fs, Ds + Fs/2, 0); % je tohle hybridni diferencovani
-        as = max3(-Fn, Dn - Fn/2, 0); 
+        ae = max([-Fe, De - Fe/2, 0]); % podle knizky strana 124 
+        aw = max([ Fw, Dw + Fw/2, 0]); % kapitola 5.7.2
+        an = max([ Fs, Ds + Fs/2, 0]); % je tohle hybridni diferencovani
+        as = max([-Fn, Dn - Fn/2, 0]); 
 
         ap = ae + aw + an + as - Sp(j,i);
         vectorOut(index) = Su(j, i);
