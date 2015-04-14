aa8c87970f80a6a057c61b8a65ab629f64538e52
Martin Kobersky <martinkobersky@gmail.com>
2015-04-15 00:20:19 +0200

    smazana mezera :D

--- correctU.m
+++ correctU.m
@@ -4,7 +4,7 @@ function ustar = correctU(pcomma, ustar, deltaY, Mu)
 for i=2:uny-1
     for j=2:unx-1
         index = (i-1)*unx + j;
-        ustar(j,i) = ustar(j,i) + (pcomma(j, i) - pcomma(j , i+1)) * (deltaY/Mu(index, index));
+        ustar(j,i) = ustar(j,i) + (pcomma(j, i) - pcomma(j, i+1)) * (deltaY/Mu(index, index));
     end
 end
 end
