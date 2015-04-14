d09f448690bea266a96d995e6ebd13becfd7a963
Martin Kobersky <martinkobersky@gmail.com>
2015-04-14 18:59:58 +0200

    posunul jsem kod

--- initUVPstar.m
+++ initUVPstar.m
@@ -7,44 +7,44 @@ if nargin == 3
 end
 
 if bounds.types.wtype == 2
-        ustar(:, 1) = bounds.u.w;
-        vstar(:, 1) = bounds.v.w;
-        pstar(:, 1) = 0;
+    ustar(:, 1) = bounds.u.w;
+    vstar(:, 1) = bounds.v.w;
+    pstar(:, 1) = 0;
 end
 if bounds.types.etype == 2
-        ustar(:, end) = bounds.u.e;
-        vstar(:, end) = bounds.v.e;
-        pstar(:, end) = 0;
+    ustar(:, end) = bounds.u.e;
+    vstar(:, end) = bounds.v.e;
+    pstar(:, end) = 0;
 end
 if bounds.types.ntype == 2
-        ustar(end, :) = bounds.u.n;
-        vstar(end, :) = bounds.v.n;
-        pstar(end, :) = 0;
+    ustar(end, :) = bounds.u.n;
+    vstar(end, :) = bounds.v.n;
+    pstar(end, :) = 0;
 end
 if bounds.types.stype == 2
-        ustar(1, :) = bounds.u.s;
-        vstar(1, :) = bounds.v.s;
-        pstar(1, :) = 0;
+    ustar(1, :) = bounds.u.s;
+    vstar(1, :) = bounds.v.s;
+    pstar(1, :) = 0;
 end
 
 if bounds.types.wtype == 0
-        ustar(:, 1) = 0;
-        vstar(:, 1) = 0;
-        pstar(:, 1) = 0;
+    ustar(:, 1) = 0;
+    vstar(:, 1) = 0;
+    pstar(:, 1) = 0;
 end
 if bounds.types.etype == 0
-        ustar(:, end) = 0;
-        vstar(:, end) = 0;
-        pstar(:, end) = 0;
+    ustar(:, end) = 0;
+    vstar(:, end) = 0;
+    pstar(:, end) = 0;
 end
 if bounds.types.ntype == 0
-        ustar(end, :) = 0;
-        vstar(end, :) = 0;
-        pstar(end, :) = 0;
+    ustar(end, :) = 0;
+    vstar(end, :) = 0;
+    pstar(end, :) = 0;
 end
 if bounds.types.stype == 0
-        ustar(1, :) = 0;
-        vstar(1, :) = 0;
-        pstar(1, :) = 0;
+    ustar(1, :) = 0;
+    vstar(1, :) = 0;
+    pstar(1, :) = 0;
 end
 end
