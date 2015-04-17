c29effd2e560f12e3a3b98e0ae00a45bfcf062f1
Martin Kobersky <martinkobersky@gmail.com>
2015-04-17 14:56:40 +0200

    zmena ve vytvareni poli

--- initUVPstar.m
+++ initUVPstar.m
@@ -1,9 +1,9 @@
 function [ustar, vstar, pstar] = initUVPstar(nx, ny, bounds, ustar, vstar, pstar)
 %inicializuje uvp pole
 if nargin == 3
-    ustar = zeros(nx + 2, ny + 1);
-    vstar = zeros(nx + 1, ny + 2);
-    pstar = zeros(nx + 2, ny + 2);
+    ustar = zeros(nx, ny + 1);
+    vstar = zeros(nx + 1, ny);
+    pstar = zeros(nx, ny);
 end
 
 if bounds.types.wtype == 2
