64d9b0ef0a389c8526bc49799d8ed76b76ec4670
Martin Kobersky <martinkobersky@gmail.com>
2015-04-17 14:54:37 +0200

    pridane spocitani rovnic pro vsechny strany

--- generateBoundaryEquations.m
+++ generateBoundaryEquations.m
@@ -1,132 +1,33 @@
-function [Mout, vectorOut] = generateBoundaryEquations(bounds, Min, vectorIn, nx, ny, allF, allD, vals)
+function [Mout, vectorOut] = generateBoundaryEquations(bounds, Min, vectorIn, nx, ny, ustar)
 % Vygeneruje Matici a vektor pravych stran pro kontrolni objemy  ktere
 % JSOU na okrajich
-
-%Von Neumanny vytahnout do vedlejsi funkce
 Mout = Min;
 vectorOut = vectorIn;
-
-
-for j=1:nx
-    for i=1:ny
-        index = (i-1)*nx + j; 
-        
-        Fe = allF(index, 1); Fw = allF(index, 2);
-        Fn = allF(index, 3); Fs = allF(index, 4);
-        
-        De = allD(index, 1); Dw = allD(index, 2);
-        Dn = allD(index, 3); Ds = allD(index, 4);
-        
-        ae = max([-Fe, De - Fe/2, 0]); % podle knizky strana 124
-        aw = max([ Fw, Dw + Fw/2, 0]); % kapitola 5.7.2
-        an = max([ Fs, Ds + Fs/2, 0]); % je tohle hybridni diferencovani
-        as = max([-Fn, Dn - Fn/2, 0]); % chtel jsem to obecneji ale asi to nejde tak lehce
-        deltaF = Fe - Fw + Fn - Fs;
-        ap = ae + aw + an + as + deltaF;
-        
-        if (i == 1 && j == 1 || i == 1 && j == nx || ...
-                i == ny && j == 1 || i == ny && j == nx) %hnus
-            Mout(index, index) = 1; %rohy
-            vectorOut(index) = 0;
-            continue;
-        end
-
-        if (i == 1 || i == ny || j == 1 || j == nx) 
-            index = (i-1)*nx + j; 
-            
-            if i == 1 % zapadni kraj
-                if bounds.w_is_d % jestli je dirichlet na zapade
-                    line = assign(index, 1, 0, 0, 0, 0, nx, ny);
-                    S = vals(j, i);
-                else % jestli je na zapade neumann
-                    ap = ae + an + as;
-                    line = assign(index, ap, an, as, ae, 0, nx, ny);
-                    S = bounds.w;
-                end
-            end
-            
-            if i == ny % vychodni kraj
-                if bounds.e_is_d % jestli je dirichlet na vychode
-                    line = assign(index, 1, 0, 0, 0, 0, nx, ny);
-                    S = vals(j, i);
-                else % jestli je na vychode neumann
-                    if j == ny - 1
-                        an = 0;
-                    end
-                    if j == 2
-                        as = 0;
-                    end
-                    ap = aw + an + as;
-                    line = assign(index, ap, an, as, 0, aw, nx, ny);
-                    S = bounds.e;
-                end
-            end
-            
-            if j == 1 % jizni kraj
-                if bounds.s_is_d % jestli je dirichlet na jihu
-                    line = assign(index, 1, 0, 0, 0, 0, nx, ny);
-                    S = vals(j, i);
-                else % jestli je na jihode neumann
-                    ap = aw + an + ae;
-                    line = assign(index, ap, an, 0, ae, aw, nx, ny);
-                    S = bounds.s;
-                end
-            end
-            
-            if j == nx % severni kraj
-                if bounds.n_is_d % jestli je dirichlet na severu
-                    line = assign(index, 1, 0, 0, 0, 0, nx, ny);
-                    S = vals(j, i);
-                else % jestli je na severu neumann
-                    ap = aw + ae + as;               
-                    line = assign(index, ap, 0, as, ae, aw, nx, ny);
-                    S = bounds.n;
-                end
-            end
-            Mout(index,1:end) = line;
-            vectorOut(index) = S;
-        end
-    end
+order = 1;
+n = nx*ny;
+for index=1:n
+    line = assign(index, 1, 0, 0, 0, 0, n, 1);
+    Mout(order, :) = line;
+    vectorOut(order) = bounds.w;
+    order = order + 1;
+end 
+for index=n-nx+1:n
+    line = assign(index, 1, 0, 0, 0, 0, n, 1);
+    Mout(order, :) = line;
+    vectorOut(order) = bounds.e;
+    order = order+1;
 end
-
+for index=1:ny:n-ny
+    line = assign(index, 1, 0, 0, 0, 0, n, 1);
+    Mout(order, :) = line;
+    vectorOut(order) = bounds.s;
+    order = order + 1;
+end 
+for index=ny:ny:n
+    line = assign(index, 1, 0, 0, 0, 0, n, 1);
+    Mout(order, :) = line;
+    vectorOut(order) = bounds.n;
+    order = order+1;
 end
-
-        
-%         if (i == 1 && j == 1)
-%             ap = an + ae;
-%             line = assign(index, ap, an, 0, ae, 0, nx, ny);
-%             S = 0;
-%             Mout(index,1:end) = line;
-%             vectorOut(index) = S;
-%             continue;
-%         end
-%         
-%         if (i == 1 && j == nx)
-%             ap = as + ae;
-%             line = assign(index, ap, 0, as, ae, 0, nx, ny);
-%             S = 0;
-%             Mout(index,1:end) = line;
-%             vectorOut(index) = S;
-%             continue;
-%         end
-%         
-%         if (i == ny && j == 1 )
-%             ap = an + aw;
-%             line = assign(index, ap, an, 0, 0, aw, nx, ny);
-%             S = 0;
-%             Mout(index,1:end) = line;
-%             vectorOut(index) = S;
-%             continue;
-%         end
-%         
-%         if (i == ny && j == nx)
-%             ap = as + aw;
-%             line = assign(index, ap, 0, as, 0, aw, nx, ny);
-%             S = 0;
-%             Mout(index,1:end) = line;
-%             vectorOut(index) = S;
-%             continue;
-%         end
-        
-        
+end
 
--- generateNonBoundaryEquations.m
+++ generateNonBoundaryEquations.m
@@ -1,32 +1,50 @@
-function [Mout, vectorOut] = generateNonBoundaryEquations(Su, Sp, allF, allD, Min, vectorIn, nx, ny, deltaX, deltaY)
+function [Mout, vectorOut] = generateNonBoundaryEquations(Su, Sp, allF, allD, Min, vectorIn, nx, ny, Icko)
 % Vygeneruje Matici a vektor pravych stran pro kontrolni objemy  ktere
 % NEjsou na okrajich
 
 Mout = Min;
 vectorOut = vectorIn;
-
-for j=2:nx-1
-    for i=2:ny-1
-        index = (i-1)*nx + j; 
-        
+index = 1;
+for i=1:ny
+    for j=1:nx
         Fe = allF(index, 1); Fw = allF(index, 2);
         Fn = allF(index, 3); Fs = allF(index, 4);
         
         De = allD(index, 1); Dw = allD(index, 2);
         Dn = allD(index, 3); Ds = allD(index, 4);
         
-        ae = max3(-Fe, De - Fe/2, 0); % podle knizky strana 124 
-        aw = max3( Fw, Dw + Fw/2, 0); % kapitola 5.7.2
-        an = max3(-Fn, Dn - Fn/2, 0); % je tohle hybridni diferencovani
-        as = max3( Fs, Ds + Fs/2, 0); 
-        deltaF = Fe - Fw + Fn - Fs;
-        ap = ae + aw + an + as - Sp(j,i) + deltaF;
+        if i == 1
+            Fw = 0;
+            Dw = 0;
+        end
+        if i == ny
+            Fe = 0;
+            De = 0;
+        end
+        if j == 1
+            Fs = 0;
+            Ds = 0;
+        end
+        if j == nx
+            Fn = 0;
+            Dn = 0;
+        end
         
+        ae = max([-Fe, De - Fe/2, 0]); % podle knizky strana 124 
+        aw = max([ Fw, Dw + Fw/2, 0]); % kapitola 5.7.2
+        an = max([ Fn, Dn + Fn/2, 0]); % je tohle hybridni diferencovani
+        as = max([-Fs, Ds - Fs/2, 0]); 
+        if i == Icko
+            ap = ae + aw + an + as;
+            vectorOut(index) = 0;
+        else
+            vectorOut(index) = Su(j, i);
+            ap = ae + aw + an + as - Sp(j,i);
+        end
         line = assign(index, ap, an, as, ae, aw, nx, ny);
-        Mout(index,1:end) = line;
-        vectorOut(index) = Su(j,i);
+        Mout(index, :) = line;
+        index = index+1;
     end
 end
-
 end
 
