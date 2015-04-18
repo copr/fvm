e306b307b1ac972e8edca07f3f63d27aadb8804a
Martin Kobersky <martinkobersky@gmail.com>
2015-04-18 17:36:30 +0200

    zmeneno aby reflektovali jinou velikost tlakoveho pole

--- calcSourcesFromPressureForV.m
+++ calcSourcesFromPressureForV.m
@@ -2,7 +2,7 @@ function SV = calcSourcesFromPressureForV(SV, pstar, vnx, vny, deltaX, deltaY)
 %napocitava zdroje vychazejici z rozdilu tlaku pro v-rovnice
 for j=2:vnx-1
     for i=2:vny-1
-        SV(j,i) = SV(j,i) + (pstar(j, i) - pstar(j+1, i))*deltaX;
+        SV(j,i) = SV(j,i) + (pstar(j-1, i-1) - pstar(j, i-1))*deltaX;
     end
 end
 end 
