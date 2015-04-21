fa4c4bed216ab5a1113aa92882372fbf15614529
Martin Kobersky <martinkobersky@gmail.com>
2015-04-21 23:51:51 +0200

    predelane spojeni matic do cyklu

--- generateSeperateMatrices.m
+++ generateSeperateMatrices.m
@@ -1,30 +1,24 @@
-function [bigMatrix, bigVector] = generateSeperateMatrices(Su, Sp, allF, allD, nx, ny, Mnx, Mny)
+function [bigMatrix, bigVector] = generateSeperateMatrices(Su, Sp, allF, allD, nx, ny, Mnx, Mny, vals)
 %vytvori velkou matici z Mnx*Mny malych matic
-[Ms, Vs, Fs, Ds, Sus, Sps] = giveMeAll(Su, Sp, allF, allD, Mnx, Mny, nx, ny);
+[Ms, Vs, Fs, Ds, Sus, Sps, indexes] = giveMeAll(Su, Sp, allF, allD, Mnx, Mny, nx, ny);
+Mb = sparse(2*(nx+ny)-2, nx*ny);
+vectorB = zeros(2*(nx+ny)-2, 1);
 bigMatrix = sparse(0,0);
 bigVector = [];
 
-for i=1:nx*ny
-    [x, y] = size(Fs(i).indexes);
+for i=1:Mnx*Mny
+    [x, y, ~] = size(Fs(i).indexes);
     [matrice, vector] = generateNonBoundaryEquations(Sus(i).indexes, Sps(i).indexes, Fs(i).indexes, Ds(i).indexes, Ms(i).indexes, Vs(i).indexes, x, y);
     bigMatrix = mergeMatrices(bigMatrix, matrice);
     bigVector = [bigVector; vector];
 end
+
     
-    
-    
-[matrice1, vector1] = generateNonBoundaryEquations(Su(1:matriceSizeX, 1:matriceSizeY), Sp(1:matriceSizeX, 1:matriceSizeY),...
-    allF(1:matriceSizeX, 1:matriceSizeY), allD(1:matriceSizeX, 1:matriceSizeY), matrice1, vector1, matriceSizeX, matriceSizeY, nx+1);
+[Mb, vectorB] = generateBoundaryEquations(Mb, vectorB, nx, ny, vals, Mnx, Mny);
+bigMatrix = mergeMatrixAndBounds(bigMatrix, Mb);
+bigVector = [bigVector; vectorB];
 
-[matrice2, vector2] = generateNonBoundaryEquations(Su(1:matriceSizeX, matriceSizeY:end), Sp(1:matriceSizeX, matriceSizeY:end),...
-    allF(1:matriceSizeX, 1:matriceSizeY), allD(matriceSizeX:end, 1:matriceSizeY), matrice1, vector1, matriceSizeX+1, matriceSizeY, nx+1);
-
-[matrice3, vector3] = generateNonBoundaryEquations(Su(matriceSizeX:end, 1:matriceSizeY), Sp(matriceSizeX:end, 1:matriceSizeY),...
-    allF(matriceSizeX:end, 1:matriceSizeY), allD(1:matriceSizeX, 1:matriceSizeY), matrice1, vector1, matriceSizeX, matriceSizeY+1, nx+1);
-
-[matrice4, vector4] = generateNonBoundaryEquations(Su(matriceSizeX:end, matriceSizeY:end), Sp(matriceSizeX:end, matriceSizeY:end),...
-    allF(matriceSizeX:end, matriceSizeY:end), allD(matriceSizeX:end, matriceSizeY:end), matrice1, vector1, matriceSizeX+1, matriceSizeY+1, nx+1);
-
-matrices = mergeMatrices(mergeMatrices(mergeMatrices(matrice1, matrice2), matrice3), matrice4);
-vectors = [vector1; vector2; vector3; vector4];
-
+[Mg, vectorG] = generateGlueEquations(Mg, vg);
+bigMatrix = mergeMatrixAndBounds(bigMatrix, Mg);
+bigVector = [bigVector; vectorG];
+end
