9e599d88cc344d0dda7c3e0aa163697722affda5
Martin Kobersky <martinkobersky@gmail.com>
2015-04-21 18:43:23 +0200

    reflektuje zmenu ve vytvareni f-ek dcek

--- generateNonBoundaryEquations.m
+++ generateNonBoundaryEquations.m
@@ -9,11 +9,11 @@ order = 1;
 for i=1:ny
     for j=1:nx
         index = (i-1)*nx + j;
-        Fe = allF(index, 1); Fw = allF(index, 2);
-        Fn = allF(index, 3); Fs = allF(index, 4);
+        Fe = allF(j, i, 1); Fw = allF(j, i, 2);
+        Fn = allF(j, i, 3); Fs = allF(j, i, 4);
         
-        De = allD(index, 1); Dw = allD(index, 2);
-        Dn = allD(index, 3); Ds = allD(index, 4);
+        De = allD(j, i, 1); Dw = allD(j, i, 2);
+        Dn = allD(j, i, 3); Ds = allD(j, i, 4);
         
         if i == 1
             Fw = 0;
@@ -36,14 +36,15 @@ for i=1:ny
         aw = max([ Fw, Dw + Fw/2, 0]); % kapitola 5.7.2
         an = max([-Fn, Dn - Fn/2, 0]); % je tohle hybridni diferencovani
         as = max([ Fs, Ds + Fs/2, 0]);
-
+        
         deltaF = Fe - Fw + Fn - Fs;
-        %         if i == Icko
-        %             ap = ae + aw + an + as;
-        %             vectorOut(index) = 0;
-        %         else
-        vectorOut(order) = Su(j, i);
-        ap = ae + aw + an + as - Sp(j,i) + deltaF;
+        if i == Icko
+            ap = ae + aw + an + as + deltaf;
+            vectorOut(index) = 0;
+        else
+            vectorOut(order) = Su(j, i);
+            ap = ae + aw + an + as - Sp(j,i) + deltaF;
+        end
         line = assign(index, ap, an, as, ae, aw, nx, ny);
         Mout(order, :) = line;
         order = order+1;
