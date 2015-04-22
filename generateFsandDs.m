bcc2f14372ab1b4094be03c2ba082c5f215b069d
Martin Kobersky <martinkobersky@gmail.com>
2015-04-23 01:27:45 +0200

    zmeneno tak aby generoval fka decka tak jak je treba

--- generateFsandDs.m
+++ generateFsandDs.m
@@ -1,22 +1,20 @@
-function [allF, allD] = generateFsandDs(n, u, v, ro, gama, deltaX, deltaY)
-allF = zeros(n, 4);
-allD = zeros(n, 4);
+function [allF, allD] = generateFsandDs(nx, ny, u, v, ro, gama, deltaX, deltaY)
+allF = zeros(nx, ny);
+allD = zeros(nx, ny);
 
-for i=1:n
-    Fw = u*ro*deltaY;
-    Fe = Fw;
-    Fs = v*ro*deltaX;
-    Fn = Fs;
-    Dw = deltaY*gama/deltaX;
-    De = Dw;
-    Ds = deltaX*gama/deltaY;
-    Dn = Ds;
-    
-    allF(i, 1) = Fe; allF(i, 2) = Fw;
-    allF(i, 3) = Fn; allF(i, 4) = Fs;
-    
-    allD(i, 1) = De; allD(i, 2) = Dw;
-    allD(i, 3) = Dn; allD(i, 4) = Ds;
+for j=1:nx
+    for i=1:ny
+        Fw = u*ro*deltaY;
+        Fe = Fw;
+        Fs = v*ro*deltaX;
+        Fn = Fs;
+        Dw = deltaY*gama/deltaX;
+        De = Dw;
+        Ds = deltaX*gama/deltaY;
+        Dn = Ds;
+        
+        allF(j, i, 1:4) = [Fe, Fw, Fn, Fs];
+        allD(j, i, 1:4) = [De, Dw, Dn, Ds];
+    end
 end
-
 end
