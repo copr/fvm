a517fff33ed40ea4e4fc6f9c6a1e8c8e53250e5d
Martin Kobersky <martinkobersky@gmail.com>
2015-04-23 01:30:21 +0200

    funkce nyni generuje i indexy na jednotlivych pozicich

--- giveMeAll.m
+++ giveMeAll.m
@@ -1,13 +1,13 @@
-function [ Ms, Vs, Fs, Ds, Sus, Sps, indexes ] = giveMeAll( Su, Sp, allF, allD, Mnx, Mny, nx, ny)
+function [ Ms, Vs, Fs, Ds, Sus, Sps, indexes] = giveMeAll( Su, Sp, allF, allD, Mnx, Mny, nx, ny)
 %vytvori rozdelene matice a vektory, pro kazdou matici vrati Fka Dcka a
 %zdroje ktere budou potreba pro vytvoreni rovnic
 %prejmenovat
 % numberOfMatrices = Mnx*Mny;
-indexes;
 matriceSizeX = nx/Mnx;
 matriceSizeY = ny/Mny;
-for i=1:Mnx
-    for j=1:Mny
+indexes = zeros(matriceSizeX + (Mnx-1)*(matriceSizeX+1), matriceSizeY + (Mny-1)*(matriceSizeY+1));
+for i=1:Mny
+    for j=1:Mnx
         index = (i-1)*Mnx + j;
         if i == 1
             My = matriceSizeY;
@@ -31,8 +31,32 @@ for i=1:Mnx
         [~, Snx] = size((j-1)*matriceSizeX+1:j*matriceSizeX);
         [~, Sny] = size((i-1)*matriceSizeY+1:i*matriceSizeY);
         
+        %zdroje pro znasobene uzly jsou nula, tady ty nuly pridavam ke
+        %zdrojum
         Sus(index).indexes = [zeros(Snx, ki) ,Su((j-1)*matriceSizeX+1:j*matriceSizeX, (i-1)*matriceSizeY+1:i*matriceSizeY); zeros(kj, ki), zeros(kj, Sny)];
         Sps(index).indexes = [zeros(Snx, ki) ,Sp((j-1)*matriceSizeX+1:j*matriceSizeX, (i-1)*matriceSizeY+1:i*matriceSizeY); zeros(kj, ki), zeros(kj, Sny)];
+        
+        inds = reshape(1:Mx*My, Mx, My) + max(max(indexes));
+        if i == 1 && j == 1
+            indexes(1:Mx, 1:My) = inds;
+        else
+%             (j-1)*Mx+(j<2)*1:j*Mx-1*(j>1)
+%             (i-1)*My+(i<2)*1:i*My-1*(i>1)
+            indexes((j-1)*Mx+(j<2)*1:j*Mx-1*(j>1), (i-1)*My+(i<2)*1:i*My-1*(i>1)) = inds;
+%             if i-2 < 2
+%                 ii = 0;
+%             else
+%                 ii = i-1;
+%             end
+%             if j-2 < 1
+%                 jj = 0;
+%             else
+%                 jj = j-1;
+%             end
+%             (j>1)*matriceSizeX + Mx*jj + 1:(j>1)*matriceSizeX + Mx*(jj+1)
+%             (i>1)*matriceSizeY + My*ii + 1:(i>1)*matriceSizeY + My*(ii+1)
+%             indexes((j>1)*matriceSizeX + Mx*jj + 1:(j>1)*matriceSizeX + Mx*(jj+1), (i>1)*matriceSizeY + My*ii + 1:(i>1)*matriceSizeY + My*(ii+1)) = inds;
+        end
     end
 end
 end
