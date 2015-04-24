797003971dd50a186bd3458b34d7e1bb31dcab1c
Martin Kobersky <martinkobersky@gmail.com>
2015-04-24 13:22:29 +0200

    pripravene pro simple po odkomentovani, pridane zjisteni flagu

--- generateSeperateMatrices.m
+++ generateSeperateMatrices.m
@@ -1,14 +1,28 @@
-function [bigMatrix, bigVector] = generateSeperateMatrices(Su, Sp, allF, allD, nx, ny, Mnx, Mny, vals)
+function [bigMatrix, bigVector, result] = generateSeperateMatrices(Su, Sp, allF, allD, nx, ny, Mnx, Mny, vals);%, alfa, olds)
 %vytvori velkou matici z Mnx*Mny malych matic
-[Ms, Vs, Fs, Ds, Sus, Sps, indexes] = giveMeAll(Su, Sp, allF, allD, Mnx, Mny, nx, ny);
+[Ms, Vs, Fs, Ds, Sus, Sps, indexes] = giveMeAll(Su, Sp, allF, allD, Mnx, Mny, nx, ny);%, olds);
 
 bigMatrix = sparse(0,0);
 bigVector = [];
-for i=1:Mnx*Mny
-    [x, y, ~] = size(Fs(i).indexes);
-    [matrice, vector] = generateNonBoundaryEquations(Sus(i).indexes, Sps(i).indexes, Fs(i).indexes, Ds(i).indexes, Ms(i).indexes, Vs(i).indexes, x, y);
-    bigMatrix = mergeMatrices(bigMatrix, matrice);
-    bigVector = [bigVector; vector];
+vlajka1 = false;
+vlajka2 = false;
+for i=1:Mny
+    for j=1:Mnx
+        index = (i-1)*Mnx + j;
+        [x, y, ~] = size(Fs(index).indexes);
+        if i > 1
+            vlajka1 = true;
+        end
+        if j > 1
+            vlajka2 = true;
+        end
+        [matrice, vector] = generateNonBoundaryEquations(Sus(index).indexes, Sps(index).indexes, Fs(index).indexes, Ds(index).indexes, Ms(index).indexes, Vs(index).indexes, x, y, vlajka1, vlajka2);
+%         [mx, my] = size(oldValues(index).indexes)
+%         [x, y] = size(Sus(index).indexes)
+%         [matrice, vector] = relax(matrice, vector, alfa, mx, my, oldValues(index).indexes);
+        bigMatrix = mergeMatrices(bigMatrix, matrice);
+        bigVector = [bigVector; vector];
+    end
 end
 [Mb, vectorB] = generateBoundaryEquations(vals, indexes);
 bigMatrix = mergeMatrixAndBounds(bigMatrix, Mb);
@@ -32,8 +46,4 @@ res = bigMatrix\bigVector;
 res = res(1:end-sizeG-sizeB);
 res = getFromRes(res, indexes);
 result = getResult(res, nx, ny, Mnx, Mny);
-figure(1)
-mesh(res)
-figure(2)
-mesh(result);
 end
