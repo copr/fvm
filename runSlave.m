b99f4ee1da89b2052a91d9174162138e23dab468
Martin Kobersky <martinkobersky@gmail.com>
2015-04-24 13:24:35 +0200

    zmena skriptu, vykresli slozenou neslozenou verzi ulohy a rozdily mezi obema

--- runSlave.m
+++ runSlave.m
@@ -1,3 +1,5 @@
+clear;
+
 v.e = 0;
 v.n = 0;
 v.s = 0;
@@ -7,18 +9,26 @@ v.n_is_d = true;
 v.s_is_d = true;
 v.w_is_d = true;
 
+n = 60;
+Mnx = 2;
+Mny = 2;
 
-n = 48;
-Mnx = 3;
-Mny = 3;
-
-Su = ones(n, n);
+Su = zeros(n, n);
+Su(2:end-1,2:end-1) = 1;
 Sp = zeros(n, n);
 
 gama = 1;
 ro = 1;
 
-fvm(0,0,1,1,gama, ro,n,n,v,Su,Sp, Mnx, Mny);
+u = 0;
 
+[res1] = fvm(u,0,1,1,gama, ro,n,n,v,Su,Sp, Mnx, Mny);
+[res2] = fvm2(u,0,1,1,gama, ro,n,n,v,Su,Sp);
+figure('name', 'slozeny')
+mesh(res1);
+figure('name', 'neslozeny')
+mesh(res2);
+figure('name', 'chyba')
+mesh(res1 - res2);
 
 
