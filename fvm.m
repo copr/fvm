e1397ab6a3884790df8c0538f35db0cd41f5810c
Martin Kobersky <martinkobersky@gmail.com>
2015-04-24 13:18:58 +0200

    predelany zpusob vypoctu na rozlozeni podoblasti

--- fvm.m
+++ fvm.m
@@ -1,4 +1,4 @@
-function [] = fvm(u, v, ro, gama, Lx, Ly, nx, ny, bounds, Su, Sp, Mnx, Mny)
+function [result] = fvm(u, v, ro, gama, Lx, Ly, nx, ny, bounds, Su, Sp, Mnx, Mny)
 %UNTITLED10 Summary of this function goes here
 %   Detailed explanation goes here
 deltaX = Lx/nx;
@@ -9,8 +9,7 @@ n = nx*ny;
 [allF,allD] = generateFsandDs(nx, ny, u, v, ro, gama, deltaX, deltaY);
 % [M, vector] = generateNonBoundaryEquations(Su, Sp, allF, allD, M, vector, nx, ny);
 % [M, vector] = generateBoundaryEquations(bounds, M, vector, nx, ny, allF, allD);
-[bigMatrix, bigVector] = generateSeperateMatrices(Su .* (-deltaX*deltaY), Sp .* deltaX*deltaY, allF, allD, nx, ny, Mnx, Mny, zeros(size(Su)));
-
+[~, ~, result] = generateSeperateMatrices(Su .* (deltaX*deltaY), Sp .* deltaX*deltaY, allF, allD, nx, ny, Mnx, Mny, zeros(size(Su)));
 % sol = bigMatrix\bigVector;
 % 
 % solMatrix = reshape(sol, nx, ny);
