92111db8a7867049685d3147c5b99a3facb91c31
Martin Kobersky <martinkobersky@gmail.com>
2015-04-29 00:26:55 +0200

    opravena chyba s dvakrat aplikovanimm realxace

--- generetaPresureCorrectEqs.m
+++ generetaPresureCorrectEqs.m
@@ -43,7 +43,7 @@ for i=1:pny
         else
             ul = ustar(obj, obi-1);
             ind = (i-2)*unx + j;
-            aw = ro*(alfa*(deltaY^2)/Mu(ind, ind));%(index-1, index-1));
+            aw = ro*((deltaY^2)/Mu(ind, ind));%(index-1, index-1));
         end
         if i > uny
             ur = ustar(obj, obi);
@@ -52,7 +52,7 @@ for i=1:pny
             ur = ustar(obj, obi);
             ind = (i-1)*unx + j;
 
-            ae = ro*(alfa*(deltaY^2)/Mu(ind,ind));%(index,index));
+            ae = ro*((deltaY^2)/Mu(ind,ind));%(index,index));
         end
         if j > vnx
             vt = vstar(obj,obi);
@@ -60,7 +60,7 @@ for i=1:pny
         else
             vt = vstar(obj, obi);
             ind = (i-1)*vnx + j;
-            an = ro*(alfa*(deltaX^2)/Mv(ind, ind));%(index-(i-1), index-(i-1)));
+            an = ro*((deltaX^2)/Mv(ind, ind));%(index-(i-1), index-(i-1)));
         end
         if j-1 < 1
             vb = vstar(obj-1, obi);
@@ -68,7 +68,7 @@ for i=1:pny
         else
             vb = vstar(obj-1, obi);
             ind = (i-1)*vnx + j -1;
-            as = ro*(alfa*(deltaX^2)/Mv(ind,ind));%(index-1-(i-1), index-1-(i-1)));
+            as = ro*((deltaX^2)/Mv(ind,ind));%(index-1-(i-1), index-1-(i-1)));
        end
         
         Source = ro*ul*deltaY - ro*ur*deltaY + ro*vb*deltaX - ro*vt*deltaX;
